6cb10a3ec646ea06306a8824d75a07b452cb05e4
Yashashwini MV <yashaswini@cambridge.edu.in>
2026-05-17 13:34:02 +0530

    fixed avatar errors

--- src/components/Avatar3D.js
+++ src/components/Avatar3D.js
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { OrbitControls, Sphere, MeshDistortMaterial, Float } from '@react-three/drei';
+import { Sphere, Float } from '@react-three/drei';
 import * as THREE from 'three';
 
 /* -------- Animated Avatar Mesh -------- */
